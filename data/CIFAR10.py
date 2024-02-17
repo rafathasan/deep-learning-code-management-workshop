c5e45f35e798457dbb24ba46db08b9436dab0629
rafathasan <rafathasankhan@gmail.com>
2024-02-17 03:54:07 +0000

    Added cross-validation

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -6,6 +6,7 @@ from torchvision import datasets, transforms
 import os
 import numpy as np
 from PIL import Image
+from torch.utils.data import Subset
 
 class CIFAR10(pl.LightningDataModule):
     def __init__(self, **kwargs):
@@ -15,6 +16,8 @@ class CIFAR10(pl.LightningDataModule):
         self.test_batch_size = kwargs.get('test_batch_size')
         self.num_workers = kwargs.get('num_workers')
         self.shuffle = kwargs.get('shuffle')
+        self.k_folds = kwargs.get('k_folds')
+        self.k = kwargs.get('k')
         self.train_transform = lambda img : A.from_dict(kwargs.get('train_transform'))(image=np.array(img))['image'] 
         self.test_transform = lambda img : A.from_dict(kwargs.get('test_transform'))(image=np.array(img))['image'] 
 
@@ -25,7 +28,19 @@ class CIFAR10(pl.LightningDataModule):
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
             train_val_data = datasets.CIFAR10(root=self.data_path, train=True, download=False, transform=self.train_transform)
-            self.train_data, self.val_data = random_split(train_val_data, [40000, 10000])
+            
+            # Calculate the size of each fold
+            fold_size = len(train_val_data) // self.k_folds
+            val_start = fold_size * self.k
+            val_end = val_start + fold_size
+            
+            # Split the data into train and validation sets
+            indices = list(range(len(train_val_data)))
+            train_indices = indices[:val_start] + indices[val_end:]
+            val_indices = indices[val_start:val_end]
+            
+            self.train_data = Subset(train_val_data, train_indices)
+            self.val_data = Subset(train_val_data, val_indices)
         
         if stage == 'test' or stage == 'predict' or stage is None:
             self.test_data = datasets.CIFAR10(root=self.data_path, train=False, download=False, transform=self.test_transform)
@@ -38,32 +53,3 @@ class CIFAR10(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_data, batch_size=self.test_batch_size, num_workers=self.num_workers)
-
-    # class Dataset(Dataset):
-    #     def __init__(self, data_path, transform=None):
-    #         self.data_path = data_path
-    #         self.transform = transform
-            
-    #         self.image_files = [f for f in os.listdir(self.data_path) if '_gt' not in f]
-            
-    #     def __len__(self):
-    #         return len(self.image_files)
-            
-    #     def __getitem__(self, idx):
-    #         image_file = self.image_files[idx]
-    #         mask_file = self.image_files[idx].replace(".png", "_gt.png")
-            
-    #         image_path = os.path.join(self.data_path, image_file)
-    #         mask_path = os.path.join(self.data_path, mask_file)
-            
-    #         image = np.array(Image.open(image_path).convert('RGB'), dtype=np.uint8)
-    #         mask = np.array(Image.open(mask_path).convert('L'), dtype=np.uint8)
-            
-    #         image[(mask == 0), :] = 0
-            
-    #         if self.transform:
-    #             augmented = self.transform(image=image, mask=mask)
-    #             image = augmented['image']
-    #             mask = augmented['mask']
-            
-    #         return image, mask.long(), idx
